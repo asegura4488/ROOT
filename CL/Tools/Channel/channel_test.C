7ff066f1950b46ddcdeba595401c60df478b6d68
asegura4488 <asegura4488@gmail.com>
2021-01-18 18:40:27 -0500

    fd

--- CL/Tools/Channel/channel_test.C
+++ CL/Tools/Channel/channel_test.C
@@ -1,21 +1,33 @@
 #ifndef CHANNEL_H
 #define CHANNEL_H
 
+#include <TF1.h>
+
 class channel{
 
 public:
-	
+
 	channel();
-	channel(double Ndata_, double Nbkg_, double Nsignal_);
+	channel(double Ndata_, double Nbkg_, double Nsignal_, double Xmin, double Xmax_);
 	~channel();
 
+	void Plot();
+	void SetPOI(double mu_);
+
 private:
+
+	double Xmin;
+	double Xmax;
 
 	double Ndata;
 	double Nbkg;
 	double Nsignal;
 
 	double mu;
+
+	// Functions
+	TF1 *FH0;
+	TF1 *FH1;
 
 };
 
@@ -25,19 +37,46 @@ channel::channel(){
 
 }
 
-channel::channel(double Ndata_, double Nbkg_, double Nsignal_): 
-Ndata(Ndata_), Nbkg(Nbkg_), Nsignal(Nsignal_){
+channel::channel(double Ndata_, double Nbkg_, double Nsignal_, double Xmin_, double Xmax_): 
+Ndata(Ndata_), Nbkg(Nbkg_), Nsignal(Nsignal_), Xmin(Xmin_), Xmax(Xmax_){
+
+	mu = 0.;
+
+	FH0 = new TF1("FH0", "TMath::Poisson(x,[0])",Xmin,Xmax);
+	FH0->SetLineColor(2);
+	FH0->SetParameter(0,Nbkg);
+
+	FH1 = new TF1("FH1", "TMath::Poisson(x,[0])",Xmin,Xmax);
+	FH1->SetLineColor(4);
+	FH1->SetParameter(0,Nbkg + mu*Nsignal);
+
 
 }
 
 channel::~channel(){}
 
+void channel::Plot(){
+	FH0->Draw("l");
+	FH1->Draw("l,same");
+}
+
+
+
+void channel::SetPOI(double mu_){
+	mu = mu_;
+	FH1->SetParameter(0,Nbkg + mu*Nsignal);
+}
+
 void channel_test(){
 
-	channel ch1 = channel(7.,4.,11.);
-
+	channel *ch1 = new channel(7.,4.,11.,-1.,30.);
+	ch1->SetPOI(1.0);
+	ch1->Plot();
 
 }
+
+
+
 
 int main(){
 	channel_test();
